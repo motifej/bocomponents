2ae9ada6bdc41095c22035058b90b6fdfae7b0bc
esraajb <jbesraa@gmail.com>
2018-06-11 16:07:19 +0300

    export formtmeplate

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ export { default as Checkbox } from "./components/inputs/checkbox";
 export { default as TextField } from "./components/inputs/text-field";
 export { default as RadioGroup } from "./components/inputs/radio-group";
 export { default as Switch } from "./components/inputs/switch";
+
+export { default as FormTemplate } from "./components/forms/";
+
 export { BuildDefaultTable } from "./components/tables/";
