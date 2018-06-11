81e2e685191ba2353da99c5ea9d7ca2dd1e2f7b7
esraajb <jbesraa@gmail.com>
2018-06-11 12:14:34 +0300

    entry point

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as SelectField } from "./inputs/select-field";
+export { default as Checkbox } from "./inputs/checkbox";
+export { default as TextField } from "./inputs/text-field";
+export { default as RadioGroup } from "./inputs/radio-group";
+export { default as Switch } from "./inputs/switch";
