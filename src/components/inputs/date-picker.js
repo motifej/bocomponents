c7ce6b8ffbeaefa092c812b4cc2ef7454567c4ef
esraajb <jbesraa@gmail.com>
2018-06-14 14:11:44 +0300

    add placeholder and label to date picker

--- src/components/inputs/date-picker.js
+++ src/components/inputs/date-picker.js
@@ -7,13 +7,16 @@ import "react-datepicker/dist/react-datepicker.css";
 const renderDatePicker = ({
     input,
     placeholder,
+    label,
     defaultValue,
     meta: { touched, error }
 }) => (
     <div>
+        <label htmlFor={input.name}>{label}</label>
         <DatePicker
             {...input}
             dateForm="MM/DD/YYYY"
+            placeholderText={placeholder}
             selected={input.value ? moment(input.value, "MM/DD/YYYY") : null}
         />
         {touched && error && <span>{error}</span>}
