54ae01f8f18608be72722c61ceae5198d0ae05fd
ymorer <ymorer@techfinancials.com>
2018-06-20 13:11:38 +0300

    label generator for range

--- src/components/inputs/range-input.js
+++ src/components/inputs/range-input.js
@@ -1,22 +1,30 @@
 import React from "react";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
+
+const generateLabel = (valuesMap , value) => {
+    current = valuesMap.filter((val)=>{
+            return val.value === value
+    })
+    return current.label
+}
+
 const RenderInputRange = ({
     input: { onChange, ...inputParams },
     ...params
 }) => {
-    console.log("...", ...params);
+    console.log("slider params", onChange)
     return (
         <InputRange
             draggableTrack
             maxValue={20}
             minValue={0}
-            formatLabel={value => (value === 1 ? "first" : value)}
-            {...inputParams}
-            {...params}
+            formatLabel={value => `${value} kg`}
             onChange={values => {
                 onChange(values);
             }}
+            {...inputParams}
+            {...params}
         />
     );
 };
