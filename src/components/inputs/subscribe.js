3703f3a8691672e802cc7663fa6ff5f4b6a6d051
esraajb <jbesraa@gmail.com>
2018-06-25 10:51:49 +0300

    fix merge conflict

--- src/components/inputs/subscribe.js
+++ src/components/inputs/subscribe.js
@@ -2,22 +2,6 @@ import { Component, createElement } from "react";
 import { isStateLess } from "./utils";
 
 export default (MUIComponent, mapProps) => {
-<<<<<<< HEAD
-	return class InputComponent extends Component {
-		getRenderedComponent() {
-			return this.refs.component;
-		}
-		render() {
-			return createElement(MUIComponent, {
-				...mapProps(this.props),
-				ref: !isStateLess(MUIComponent)
-					? el => (this.component = el)
-					: null
-			});
-		}
-	};
-	InputComponent.displayName = `MUIRF${MUIComponent.name}`;
-=======
     return class InputComponent extends Component {
         getRenderedComponent() {
             return this.refs.component;
@@ -31,5 +15,4 @@ export default (MUIComponent, mapProps) => {
             });
         }
     };
->>>>>>> e7090be0b8326786c865e6cda29fb80932c64b15
 };
